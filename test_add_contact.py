d7130ce83c9bcecba10b723981dcea585548276a
IrinaBaB <irina.babicheva04@gmail.com>
2018-11-06 21:43:10 +1100

    adding initial test - create contact

---
+++ test_add_contact.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+
+class CreateContact(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.katalon.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_create_contact(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        driver.find_element_by_link_text("add new").click()
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("firstname").send_keys("Contact3")
+        driver.find_element_by_name("middlename").click()
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("middlename").send_keys("NN")
+        driver.find_element_by_name("lastname").click()
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("lastname").send_keys("Borisovich")
+        driver.find_element_by_name("nickname").click()
+        driver.find_element_by_name("nickname").clear()
+        driver.find_element_by_name("nickname").send_keys("borka")
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").clear()
+        driver.find_element_by_name("company").send_keys("boOne")
+        driver.find_element_by_name("address").click()
+        driver.find_element_by_name("address").clear()
+        driver.find_element_by_name("address").send_keys("23Kittiwake drive")
+        driver.find_element_by_name("theform").click()
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("borka@mail.com")
+        driver.find_element_by_name("phone2").click()
+        driver.find_element_by_name("phone2").clear()
+        driver.find_element_by_name("phone2").send_keys("67464646464")
+        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        driver.find_element_by_link_text("home").click()
+        driver.find_element_by_link_text("Logout").click()
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
