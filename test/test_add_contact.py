4f8cedf6431372758dfaa45aac0d73000e7944ec
IrinaBaB <irina.babicheva04@gmail.com>
2018-11-10 22:33:30 +1100

    Created Python packages for different purposes and  definitions

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 import pytest
 
 
