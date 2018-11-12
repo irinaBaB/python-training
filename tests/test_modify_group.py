d668c2a08e9d6b8b64927c97e99eed76cb76c254
IrinaBaB <irina.babicheva04@gmail.com>
2018-11-12 21:45:33 +1100

    adding tests Modify_group

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_modify_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="test123", header="fgfgfgfgf", footer="ffgfgfgfg"))
+    app.group.modify_first_group(Group(name="Lavanda_tree"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(footer="updatedValue"))
     app.session.logout()
