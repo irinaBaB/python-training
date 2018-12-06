ef30ad5b4ccc2bbfd52325b15d2483af0ef37658
IrinaBaB <irina.babicheva04@gmail.com>
2018-12-07 09:25:27 +1100

    updated modal\contact.py

--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,16 @@ from sys import maxsize
 
 class Contact:
 
-     def __init__(self, firstname=None,middlename=None,lastname=None,nickname=None, companyname=None,address=None,email=None,phone2=None,mobile=None,id= None):
+     def __init__(self, firstname=None,
+                  middlename=None,
+                  lastname=None,
+                  nickname=None,
+                  companyname=None,
+                  address=None,
+                  email=None,
+                  phone2=None,
+                  mobile=None,
+                  id= None):
             self.firstname = firstname
             self.middlename = middlename
             self.lastname = lastname
