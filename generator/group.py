4737259ababc1d697cc03c6e0b933bce7abf1bad
IrinaBaB <irina.babicheva04@gmail.com>
2018-12-19 16:09:55 +1100

    adding logic for reading data from json file for groups only for now

--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -35,7 +35,8 @@ testdata=[
     for i in range(n)
 ]
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json")
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata,default=lambda x: x.__dict__,indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
